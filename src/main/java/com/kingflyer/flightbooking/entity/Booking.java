7912860558b7799e8824e8a0e7d614ceb22e86bf
Uday kiran <putchakatla.uday-kiran@capgemini.com>
2021-11-11 11:00:16 +0530

    trying to merge

--- src/main/java/com/kingflyer/flightbooking/entity/Booking.java
+++ src/main/java/com/kingflyer/flightbooking/entity/Booking.java
@@ -1,5 +1,7 @@
 package com.kingflyer.flightbooking.entity;
 
 public class Booking {
-
+	https://github.com/behxaad/flightbooking.git
+		https://github.com/udaykirancg/flightbooking.git
+			4A25-B46D
 }
